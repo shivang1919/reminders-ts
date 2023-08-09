a8687679dc8930e1487ad911fe644949af529092
shivang <shivangnov@gmail.com>
2023-08-09 12:26:48 +0530

    adding app

---
+++ reminders-app/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/Reminder'
+import reminderService from './service/Reminder'
+import NewReminder from './components/NewReminder';
+interface NewReminderProps{
+
+}
+
+function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+  useEffect(() => {
+    loadReminders();
+  }, []);
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders)
+  }
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter(reminder => reminder.id !== id));
+  }
+  const addReminder = async(title:string)=>{
+    const newReminder = await reminderService.addReminder(title);
+    setReminders([newReminder,...reminders])
+  }
+  return (
+    <div className="App">
+      <NewReminder onAddReminder={addReminder}/>
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
+    </div>
+  );
+}
+
+export default App;
